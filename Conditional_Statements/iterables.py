7a4962c5d3b602fa7fb5856033bd9e156f23233b
robertelias <robert.elias@live.com>
2021-02-16 12:06:30 -0800

    updates to iterables.py

---
+++ Conditional_Statements/iterables.py
@@ -0,0 +1,18 @@
+print("==========================================")
+cities = ['new york city', 'mountain view', 'chicago', 'los angeles']
+for city in cities:
+    print(city)
+print("Done!")
+
+print("==========================================")
+
+
+for index in range(len(cities)):
+    cities[index] = cities[index].title()
+    print(cities)
+
+print("==========================================")
+for i in range(3):
+    print("Hello, Robert Elias!")
+
+
