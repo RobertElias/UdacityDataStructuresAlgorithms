ee7fd6789608cc43e949fd673fa386a0eb2552e1
robertelias <robert.elias@live.com>
2021-02-16 12:14:49 -0800

    updates

--- Conditional_Statements/iterables.py
+++ Conditional_Statements/iterables.py
@@ -15,4 +15,16 @@ print("==========================================")
 for i in range(3):
     print("Hello, Robert Elias!")
 
+print("==========================================")
+sentence = ["the", "quick", "brown", "fox",
+            "jumped", "over", "the", "lazy", "dog"]
 
+# Write a for loop to print out each word in the sentence list, one word per line
+
+for word in sentence:
+    print(word)
+
+print("==========================================")
+# Write a for loop using range() to print out multiples of 5 up to 30 inclusive
+for i in range(5, 35, 5):
+    print(i)
