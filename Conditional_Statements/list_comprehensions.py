8abf3254a7046951b555bd0c8dd6b8f9b054e29f
robertelias <robert.elias@live.com>
2021-02-22 11:39:53 -0800

    completed list comprehension exercises

--- Conditional_Statements/list_comprehensions.py
+++ Conditional_Statements/list_comprehensions.py
@@ -9,3 +9,21 @@ first_names = [name.split()[0].lower() for name in names]
 print(first_names)
 
 
+# Quiz: Multiples of Three
+# Use a list comprehension to create a list multiples_3 containing the first 20 multiples of 3.
+
+multiples_3 = [nums * 3 for nums in range(1,21)]
+print(multiples_3)
+
+
+scores = {
+    "Rick Sanchez": 70,
+    "Morty Smith": 35,
+    "Summer Smith": 82,
+    "Jerry Smith": 23,
+    "Beth Smith": 98
+}
+
+# write your list comprehension here
+passed = [name for name, score in scores.items() if score >= 65]
+print(passed)
