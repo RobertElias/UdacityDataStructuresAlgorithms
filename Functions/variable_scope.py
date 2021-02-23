5161ad0df417c7ea8a741b837497501f1ccdb7d8
robertelias <robert.elias@live.com>
2021-02-23 10:13:07 -0800

    completed variable scope exercise

---
+++ Functions/variable_scope.py
@@ -0,0 +1,79 @@
+# # This works fine
+# def some_function():
+#     word = "hello"
+#     print(word)
+
+
+
+
+# def another_function():
+#     word = "goodbye"
+#     print(word)
+
+# # print(another_function())
+# another_function()
+# # This works fine
+# word = "hello"
+
+
+# def some_function():
+#     print(word)
+
+
+# some_function()
+
+
+# print("---------------------------------")
+
+
+# egg_count = 0
+
+
+# def buy_eggs():
+#     egg_count += 12  # purchase a dozen eggs
+
+
+# buy_eggs()
+
+print("--------------------------------------")
+str1 = 'Functions are important programming concepts.'
+
+
+def print_fn():
+    str1 = 'Variable scope is an important concept.'
+    print(str1)
+
+
+print_fn()
+
+print("--------------------------------------")
+
+str1 = 'Functions are important programming concepts.'
+
+
+def print_fn():
+    #str1 = 'Variable scope is an important concept.'
+    print(str1)
+
+
+print_fn()
+
+
+print("---------------------------------")
+
+#TypeERROR
+# def print_fn():
+#     str1 = 'Variable scope is an important concept.'
+#     print(str1)
+
+
+# print_fn(str1)
+
+str1 = 'Functions are important programming concepts.'
+
+
+def print_fn():
+    print(str1)
+
+
+print_fn(str1)
