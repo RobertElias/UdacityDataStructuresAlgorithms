21dca6ab03de1294eda16fa0c83105e2d71c513b
robertelias <robert.elias@live.com>
2021-02-23 10:55:21 -0800

    scripting directory created

---
+++ Script_Folder/snake.py
@@ -0,0 +1,27 @@
+how_many_snakes = 1
+snake_string = """
+Welcome to Python3!
+
+             ____
+            / . .\\
+            \  ---<
+             \  /
+   __________/ /
+-=:___________/
+
+<3, Juno
+"""
+
+
+print(snake_string * how_many_snakes)
+
+
+name = input("Enter your name: ")
+print("Hello there, {}!".format(name.title()))
+
+
+num = int(input("Enter an integer: "))
+print("hello " * num)
+
+result = eval(input("Enter an expression: "))
+print(result)
