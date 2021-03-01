ccc840e8d8bdeae4d897f121d5725ce56a4685d6
robertelias <robert.elias@live.com>
2021-03-01 09:21:49 -0800

    is circular linked list

---
+++ Arrays_Lists/list_loop.py
@@ -0,0 +1,36 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, init_list=None):
+        self.head = None
+        if init_list:
+            for value in init_list:
+                self.append(value)
+
+    def append(self, value):
+        if self.head is None:
+            self.head = Node(value)
+            return
+
+        # Move to the tail (the last node)
+        node = self.head
+        while node.next:
+            node = node.next
+
+        node.next = Node(value)
+        return
+
+
+list_with_loop = LinkedList([2, -1, 3, 0, 5])
+
+# Creating a loop where the last node points back to the second node
+loop_start = list_with_loop.head.next
+
+node = list_with_loop.head
+while node.next:
+    node = node.next
+node.next = loop_start
